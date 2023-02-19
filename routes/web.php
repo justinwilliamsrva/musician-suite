e1f0e15717a0abd0b9d78a82b36727e3f187d49b
JUSTIN C WILLIAMS <justinwdev@gmail.com>
2023-02-18 21:22:21 -0500

    Add routes and gig controller. (#8)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\GigController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,15 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    });
+
+    Route::get('/musician-finder', [GigController::class, 'index'])->name('musician-finder.dashboard');
+    Route::resource('/gigs', GigController::class)->except('index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
