8230765914a5b7e0c4a5a52410f75ab4b410b668
Justin Williams <justinwdev@gmail.com>
2023-02-26 08:21:10 -0500

    Add factories to seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Gig;
+use App\Models\Job;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Database\Factories\JobUserFactory;
+use Database\Factories\UserJobFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +19,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(5)->create();
+        Gig::factory(10)->create();
+        Job::factory(20)->create();
+        User::all()->each(function ($user) {
+            $jobs = Job::inRandomOrder()->take(40)->get();
+            foreach ($jobs as $job) {
+                JobUserFactory::new()->create([
+                    'user_id' => $user->id,
+                    'job_id' => $job->id,
+                ]);
+            }
+        });
     }
 }
