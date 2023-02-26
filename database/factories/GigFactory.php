fe292597679c5fb12f663b690bedb7e8c76fa66a
Justin Williams <justinwdev@gmail.com>
2023-02-26 08:20:51 -0500

    Add job, gig and user and pivot table factories.

--- database/factories/GigFactory.php
+++ database/factories/GigFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use DateTime;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,20 @@ class GigFactory extends Factory
      */
     public function definition()
     {
+        $start_time = fake()->dateTimeBetween('now', '+1 year');
+        $end_time = new DateTime($start_time->format('Y-m-d H:i:s'));
+        $end_time->modify('+3 hours');
+
         return [
-            //
+            'event_type' => fake()->randomElement(['Wedding', 'Concert', 'Party']),
+            'street_address' => fake()->streetAddress(),
+            'city' => fake()->city(),
+            'state' => fake()->state(),
+            'zip_code' => fake()->postcode(),
+            'start_time' => $start_time,
+            'end_time' => $end_time,
+            'description' => fake()->optional()->text,
+            'user_id' => User::inRandomOrder()->first(['id']),
         ];
     }
 }
