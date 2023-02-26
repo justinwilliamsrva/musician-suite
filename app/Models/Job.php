2a5e7407af165350ffb00085c384f233bf44ca88
Justin Williams <justinwdev@gmail.com>
2023-02-26 08:20:04 -0500

    Add job, gig and user and pivot table models.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,10 +2,57 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'instrument_list',
+        'payment',
+        'extra_info',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+    ];
+
+    /**
+     * Get the Users for the Job.
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'job_user')->withPivot('status');
+    }
+
+    /**
+     * Get the Gig who owns the Job.
+     */
+    public function gig(): BelongsTo
+    {
+        return $this->belongsTo(Gig::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use App\Models\Gig;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -21,6 +24,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone_number',
+        'instruments',
+        'admin',
     ];
 
     /**
@@ -41,4 +47,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the Gigs for the User.
+     */
+    public function gigs(): HasMany
+    {
+        return $this->hasMany(Gig::class);
+    }
+
+    /**
+     * Get the Jobs for the User.
+     */
+    public function jobs(): BelongsToMany
+    {
+        return $this->belongsToMany(Job::class, 'job_user')->withPivot('status');
+    }
 }
