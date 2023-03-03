de95f7cfe26a29e862b3444833839a026d9fd56b
Justin Williams <justinwdev@gmail.com>
2023-03-03 18:51:40 -0500

    Replace instrument config with gigis config.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'phone_number' => fake()->phoneNumber(),
-            'instruments' => json_encode(Arr::random(config('instruments.instruments'), 3)),
+            'instruments' => json_encode(Arr::random(config('gigs.instruments'), 3)),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
