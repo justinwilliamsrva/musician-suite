97e31a1d85ed37d5f26efd2370845fd73c8e99d9
Justin Williams <justinwdev@gmail.com>
2023-03-11 15:11:24 -0500

    Change postal code to zip_code.

--- resources/views/musician-finder/create.blade.php
+++ resources/views/musician-finder/create.blade.php
@@ -78,9 +78,9 @@
                                     </select>
                                 </div>
                                 <div class="col-span-6 sm:col-span-3 lg:col-span-1">
-                                    <label for="postal_code" class="block text-sm font-medium text-gray-700">ZIP /
+                                    <label for="zip_code" class="block text-sm font-medium text-gray-700">ZIP /
                                         Postal code</label>
-                                    <input value="{{ old('postal_code') }}" type="text" name="postal_code" id="postal-code"
+                                    <input value="{{ old('zip_code') }}" type="text" name="zip_code" id="postal-code"
                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                 </div>
                                 <!-- ROW 3 -->
