142a53a01c73024cbf766916bbeb94c8d03b2c2e
Justin Williams <justinwdev@gmail.com>
2023-03-14 00:31:46 -0400

    Add more space in footer.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="sticky bottom-0 h-20 w-full bg-[#212121] p-4 mt-12">
     <p class="h-full flex items-center justify-center text-[#7B7C7C]">
-        &copy;&nbsp;
+        &copy;&nbsp;&nbsp;
         <a href="https://github.com/justinwilliamsrva" target="_blank" class="text-[#F26D5C]">Justin Williams</a>&nbsp;
         {{ date('Y') }}
     </p>
