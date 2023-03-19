550252d94f810edf2191a1ee9c866d51161571fa
Justin Williams <justinwdev@gmail.com>
2023-03-18 21:40:01 -0400

    Change language.

--- resources/views/musician-finder/edit.blade.php
+++ resources/views/musician-finder/edit.blade.php
@@ -168,7 +168,7 @@
                         Delete Gig
                     </button>
                     <button type="submit" id="update-gig-button" class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Update
+                        Save
                     </button>
                     </div>
                 </div>
