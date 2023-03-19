b6e5c3142bd7ef23c4be13dcf51c8ba806105ab1
Justin Williams <justinwdev@gmail.com>
2023-03-18 21:39:35 -0400

    Make heading smaller.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
                     @endif
                 <!-- Header -->
                 @if (isset($header))
-                    <div class="py-12">
+                    <div class="py-6">
                         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <div class="overflow-hidden">
                                 <div class="p-6 text-gray-900">
