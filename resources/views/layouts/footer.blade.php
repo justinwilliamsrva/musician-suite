ae5c7ccf7ee3ccd8afa002e73bd675d1eeba6714
Justin Williams <justinwdev@gmail.com>
2023-03-21 06:42:15 -0400

    Add new footer.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,11 @@
 <div class="sticky bottom-0 h-20 w-full bg-[#212121] p-4 mt-12">
-    <p class="h-full flex items-center justify-center text-[#7B7C7C]">
+    <p class="flex items-center justify-center text-[#7B7C7C]">
         &copy;&nbsp;&nbsp;
-        <a href="https://github.com/justinwilliamsrva" target="_blank" class="text-[#F26D5C]">Justin Williams</a>&nbsp;
+        <a href="https://www.classicalrevolutionrva.com" target="_blank" class="text-[#F26D5C]">Classical Revolution RVA</a>&nbsp;
         {{ date('Y') }}
+    </p>
+    <p class="flex items-center justify-center text-[#7B7C7C]">
+        Built By&nbsp;
+        <a href="https://github.com/justinwilliamsrva" target="_blank" class="text-[#F26D5C]">Justin Williams</a>&nbsp;
     </p>
 </div>
