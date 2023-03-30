62456c7af3f30be3c0b23b7a7032326e3e03bd98
Justin Williams <justinwdev@gmail.com>
2023-03-30 09:44:16 -0400

    Center footer.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,15 @@
 <div class="sticky bottom-0 h-24 w-full bg-[#212121] p-4 mt-12 space-y-2">
     <div class="flex items-center justify-center space-x-4">
-        <x-nav-link href="mailto:info@classicalconnectionrva.com" class="pt-0">
-            {{ __('EMAIL') }}
-        </x-nav-link>
-        <x-nav-link href="https://www.facebook.com/profile.php?id=100091524604886" target="_blank" class="pt-0">
-            {{ __('FACEBOOK') }}
-        </x-nav-link>
+        <div class="flex justify-end w-32">
+            <x-nav-link href="mailto:info@classicalconnectionrva.com" class="pt-0">
+                {{ __('EMAIL') }}
+            </x-nav-link>
+        </div>
+        <div class="flex justify-start w-32">
+            <x-nav-link href="https://www.facebook.com/profile.php?id=100091524604886" target="_blank" class="pt-0">
+                {{ __('FACEBOOK') }}
+            </x-nav-link>
+        </div>
     </div>
     <p class="flex items-center justify-center text-[#7B7C7C]">
         &copy;&nbsp;&nbsp;
