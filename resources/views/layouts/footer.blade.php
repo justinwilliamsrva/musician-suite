f4fa982a31616fad8b2f1b9998db3ccb65847838
Justin Williams <justinwdev@gmail.com>
2023-03-30 01:37:13 -0400

    Add links to footer.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,16 @@
-<div class="sticky bottom-0 h-20 w-full bg-[#212121] p-4 mt-12">
-    <p class="flex items-center justify-center h-full text-[#7B7C7C]">
+<div class="sticky bottom-0 h-20 w-full bg-[#212121] p-4 mt-12 space-y-2">
+    <div class="flex items-center justify-center space-x-4">
+        <x-nav-link href="https://github.com/justinwilliamsrva/musician-suite-cr">
+            {{ __('GITHUB') }}
+        </x-nav-link>
+        <x-nav-link href="mailto:info@classicalconnectionrva.com">
+            {{ __('EMAIL') }}
+        </x-nav-link>
+        <x-nav-link href="https://www.facebook.com/profile.php?id=100091524604886">
+            {{ __('FACEBOOK') }}
+        </x-nav-link>
+    </div>
+    <p class="flex items-center justify-center text-[#7B7C7C]">
         &copy;&nbsp;&nbsp;
         <a href="https://www.classicalrevolutionrva.com" target="_blank" class="text-[#F26D5C]">Classical Revolution RVA</a>&nbsp;
         {{ date('Y') }}
