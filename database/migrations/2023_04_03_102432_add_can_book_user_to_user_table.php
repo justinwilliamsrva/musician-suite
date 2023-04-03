c0c49a01d8a143110669a9dc8653e30eab6d1b90
Justin Williams <justinwdev@gmail.com>
2023-04-03 06:55:07 -0400

    Add can book radio to profile edit.

--- database/migrations/2023_04_03_102432_add_can_book_user_to_user_table.php
+++ database/migrations/2023_04_03_102432_add_can_book_user_to_user_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-                $table->boolean('can_book')->default(1);
+                $table->boolean('can_book')->default(true);
             });
     }
 
