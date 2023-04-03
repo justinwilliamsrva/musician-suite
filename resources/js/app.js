837857ce1fee72ab07a8c2b494e90e96a74f8085
Justin Williams <justinwdev@gmail.com>
2023-04-03 17:52:45 -0400

    Add alert message.

--- resources/js/app.js
+++ resources/js/app.js
@@ -83,7 +83,7 @@ $(document).ready(function() {
                     url: `${window.location.origin}/musician-select?number=${e.target.dataset.number}`,
                     type: 'GET',
                     complete: function() {
-                        alert('Please do not select a musician without first confirming with them directly.');
+                        alert('Please do not select a specific musician without first confirming with them directly.');
                     },
                     success: function(result) {
                         $(e.target).after(result);
